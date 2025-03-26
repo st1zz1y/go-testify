7e424eb5b236526e099b531625b32098cd17ee25
st1zz1y <shirokov966@gmail.com>
2025-03-26 17:35:29 +0300

    SC

---
+++ precode_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainHandlerCorrectRequest(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/cafe?city=moscow&count=2", nil)
+	rr := httptest.NewRecorder()
+	mainHandle(rr, req)
+
+	require.Equal(t, http.StatusOK, rr.Code, "Код ответа не 200")
+	assert.NotEmpty(t, rr.Body.String(), "Тело ответа пустое")
+}
+
+func TestMainHandlerWrongCity(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/cafe?city=london&count=2", nil)
+	rr := httptest.NewRecorder()
+	mainHandle(rr, req)
+
+	require.Equal(t, http.StatusBadRequest, rr.Code, "Код ответа не 400")
+	assert.Equal(t, "wrong city value", rr.Body.String(), "Ошибка")
+}
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/cafe?city=moscow&count=10", nil)
+	rr := httptest.NewRecorder()
+	mainHandle(rr, req)
+
+	require.Equal(t, http.StatusOK, rr.Code, "Код ответа не 200")
+
+	cafes := strings.Split(rr.Body.String(), ",")
+	assert.Len(t, cafes, 4, "Количество кафе не равно 4")
+}
